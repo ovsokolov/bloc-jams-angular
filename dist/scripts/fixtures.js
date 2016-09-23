8ad5ff23f3b2b6ad5ebb324ded14046d289dd5b1
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-09-23 05:59:18 -0400

    Angular Checkpoint 5 controllers:completed

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,47 @@
+
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // Scorpins Album
+ var albumScorpions = {
+     title: 'Blackout',
+     artist: 'Scorpions',
+     label: 'Mercury ',
+     year: '1982',
+     albumArtUrl: 'assets/images/album_covers/22.png',
+     songs: [
+         { title: 'Blackout', duration: '3:49' },
+         { title: "Can't Live Without You", duration: '3:47' },
+         { title: 'No One Like You', duration: '3:57'},
+         { title: 'You Give Me All I Need', duration: '3:39' },
+         { title: 'Now!', duration: '2:35'}
+     ]
+ };
