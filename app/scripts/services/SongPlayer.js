8327d0273a24f2b52814425ba13130868acbd65c
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-09-24 13:47:23 -0400

    Angular Checkpoint 7 Services 2:completed

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,61 @@
+(function() {
+    function SongPlayer() {
+        var player = {};
+
+        var currentSong = null;
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+        var currentBuzzObject = null;
+
+        
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+
+            currentSong = song;
+         };
+        
+        
+         player.play = function(song){
+            if (currentSong !== song) {
+                setSong(song);
+
+                currentBuzzObject.play(); 
+                song.playing = true;
+                
+            } else if (currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    currentBuzzObject.play();
+                    song.playing = true;
+                }
+            }
+         };
+        
+        
+         player.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+         };
+        
+          
+        return player;
+    }
+ 
+     angular
+         .module('blocJams')
+         .factory('SongPlayer', SongPlayer);
+ })();
--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,12 @@
 (function() {
-     function AlbumCtrl(Fixtures) {
+     function AlbumCtrl(Fixtures, SongPlayer) {
         console.log("in album");
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
         console.log( this.albumData);
      }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
  })();
